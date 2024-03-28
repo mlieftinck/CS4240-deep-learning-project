bd3ba0e2542fd2bb5ed65f6ba72507d248f7fb75
mlieftinck <martijn.lieftinck@hotmail.nl>
2024-03-28 14:23:59 +0100

    fix for multiple time series as input

--- code/main.py
+++ code/main.py
@@ -4,17 +4,22 @@ from model import nsn
 import torch.nn as nn
 from load_data import preprocessing
 from view_images import view_image
+from loss import DiceLoss
 
 
 if __name__ == "__main__":
-    image, labels = preprocessing()
+    images, labels = preprocessing()
     print(f'------------ RUNNING ------------')
+    # Try for only a singe timeseries
+    image = images[0]
+
     image = torch.unsqueeze(image, 0)
     image = image.float()
     labels = labels.long()
     net = nsn()
     optimizer = torch.optim.SGD(net.parameters(), lr=5e-1)
     criterion = nn.CrossEntropyLoss()
+    # criterion = DiceLoss()
     train_loss, train_acc = train(image, labels, net, optimizer, criterion)
     test_loss, test_acc, output_array = test(image, labels, net, criterion)
     print(output_array.size())
