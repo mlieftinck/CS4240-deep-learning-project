cc8e31999fd60dc0f700b4d794b0582772eb0fb1
mlieftinck <martijn.lieftinck@hotmail.nl>
2024-04-02 10:43:47 +0200

    different output channel for loss calc, runtime stopwatch

--- code/main.py
+++ code/main.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 from load_data import preprocessing
 from view_images import view_image
 from loss import DiceLoss
+import time
 
 
 if __name__ == "__main__":
@@ -16,11 +17,14 @@ if __name__ == "__main__":
     labels = labels.long()
     net = ndn()
     optimizer = torch.optim.Adam(net.parameters(), lr=5e-1)
-    criterion = nn.CrossEntropyLoss()
+    # criterion = nn.CrossEntropyLoss()
     criterion = DiceLoss()
+    time_start = time.time()
     train_loss, train_acc = train(images, labels, net, optimizer, criterion)
     test_loss, test_acc, output_array = test(images, labels, net, criterion)
+    time_end = time.time() - time_start
     print(output_array.size())
     print(f'test loss: {test_loss}, test accuracy = {test_acc}')
+    print(f"Total elapsed time: {time_end // 60:.0f} min {time_end % 60:.0f} sec")
     view_image(output_array.cpu().detach().numpy()[0])
     # view_image(labels[0].cpu().detach().numpy()[0])
