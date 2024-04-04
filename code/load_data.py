552ab6c831102ba075fabf075070cc0a4d8b92c7
mlieftinck <martijn.lieftinck@hotmail.nl>
2024-04-04 15:18:59 +0200

    clean preprocessing, manual folder creation

--- code/load_data.py
+++ code/load_data.py
@@ -2,6 +2,7 @@ import os
 from PIL import Image
 import numpy as np
 import torch
+import time
 
 
 def try_gpu():
@@ -17,19 +18,16 @@ def try_gpu():
 
 
 def preprocessing(source_folder="data/input/train"):
-    i = source_folder[4:]
-    os.chdir('..')
+    start_time = time.time()
+    slices_per_image = 51
+    images_per_timeseries = 11
     project_dir = os.getcwd()
 
     # Upload images from data folder
     data_path = os.path.join(project_dir, source_folder)
-    # file_dir_test = os.path.join(project_dir, 'data/labels/train/' + model + '/')
 
     image_timeseries = []
-    batch_of_truths = []
-
     batches = []
-    epochs_of_truths = []
 
     data_dir = os.listdir(data_path)
     data_dir.sort()
@@ -41,7 +39,7 @@ def preprocessing(source_folder="data/input/train"):
         image_list = []
 
         # Append all the pixel values of the image into one (51, 114, 112) array (z, y, x)
-        for i in range(51):
+        for i in range(slices_per_image):
             img.seek(i)
             array_img = np.array(img)
             image_list.append(array_img)
@@ -66,8 +64,7 @@ def preprocessing(source_folder="data/input/train"):
 
         image_timeseries.append(image_tensor_normalized)
 
-        print("pre-flip finished!")
-        if len(image_timeseries) == 11:
+        if len(image_timeseries) == images_per_timeseries:
             image_timeseries_flipped_x = []
             image_timeseries_flipped_y = []
             image_timeseries_flipped_xy = []
@@ -85,81 +82,30 @@ def preprocessing(source_folder="data/input/train"):
             batches.append(image_timeseries_flipped_x)
             batches.append(image_timeseries_flipped_y)
             batches.append(image_timeseries_flipped_xy)
-
             image_timeseries = []
-            image_timeseries_flipped_x = []
-            image_timeseries_flipped_y = []
-            image_timeseries_flipped_xy = []
 
     batches = np.array(batches)
     batches = torch.Tensor(batches)
 
-    # for testing_file in test_directory:
-    #     lab = Image.open(file_dir_test + testing_file)
-    #
-    #     # Empty list to read pixel values of original image
-    #     array_of_truth = []
-    #
-    #     # Append all the pixel values of the image in to one (51, 114, 112) array (z, y, x)
-    #     for i in range(51):
-    #         lab.seek(i)
-    #         array_lab = np.array(lab)
-    #         array_of_truth.append(array_lab)
-    #
-    #     array_of_truth = np.array(array_of_truth, dtype=float)
-    #     array_of_truth_tensor = torch.tensor(array_of_truth)
-    #
-    #     array_of_truth_tensor = torch.unsqueeze(array_of_truth_tensor, 0)
-    #     array_of_truth_tensor = torch.unsqueeze(array_of_truth_tensor, 0)
-    #
-    #     array_of_truth_tensor_interpolated = torch.nn.functional.interpolate(array_of_truth_tensor,
-    #                                                                          size=(112, 114, 112),
-    #                                                                          mode='nearest') / 255
-    #
-    #     reflection_pad_3d = torch.nn.ReflectionPad3d((8, 8, 7, 7, 8, 8))
-    #     array_of_truth_tensor_interpolated_padded = reflection_pad_3d(array_of_truth_tensor_interpolated)
-    #     array_of_truth_tensor_interpolated_padded = torch.squeeze(array_of_truth_tensor_interpolated_padded, 0)
-    #     array_of_truth_tensor_interpolated_padded = torch.squeeze(array_of_truth_tensor_interpolated_padded, 0)
-    #
-    #     batch_of_truths.append(array_of_truth_tensor_interpolated_padded)
-    #
-    #     if len(batch_of_truths) == 11:
-    #         image_flipped_x_list = []
-    #         image_flipped_y_list = []
-    #         image_flipped_xy_list = []
-    #
-    #         for pre_flipped_image in batch_of_truths:
-    #             image_flipped_x = torch.flip(pre_flipped_image, [0])
-    #             image_flipped_y = torch.flip(pre_flipped_image, [1])
-    #             image_flipped_xy = torch.flip(image_flipped_x, [1])
-    #
-    #             image_flipped_x_list.append(image_flipped_x)
-    #             image_flipped_y_list.append(image_flipped_y)
-    #             image_flipped_xy_list.append(image_flipped_xy)
-    #
-    #         epochs_of_truths.append(batch_of_truths)
-    #         epochs_of_truths.append(image_flipped_x_list)
-    #         epochs_of_truths.append(image_flipped_y_list)
-    #         epochs_of_truths.append(image_flipped_xy_list)
-    #
-    #         batch_of_truths = []
-    #         image_flipped_x_list = []
-    #         image_flipped_y_list = []
-    #         image_flipped_xy_list = []
-    #
-    # epochs_of_truths = np.array(epochs_of_truths)
-    # epochs_of_truths = torch.Tensor(epochs_of_truths)
+    # os.makedirs(project_dir + "/preprocessed_data/" + source_folder[4:] + "/", exist_ok=True)
+    torch.save(batches, project_dir + "/preprocessed_data" + source_folder[4:] + ".pt")
 
-    os.makedirs(project_dir + "/preprocessed_data/" + source_folder[4:] + "/", exist_ok=True)
-    torch.save(batches, "/preprocessed_data" + source_folder[4:] + ".pt")
-    # torch.save(epochs_of_truths,
-    #            '/Users/aman/Desktop/TUDelft Year 5/Deep Learning/Project/CS4240-deep-learning-project/data/preprocessed_data/epochs_of_truths_' + model + '.pt')
-
-    return batches
+    return time.time() - start_time
 
 
 if __name__ == "__main__":
     device = try_gpu()
     print(device)
-    batch = preprocessing()
-    print(batch.size())
+    os.chdir('..')
+    t = preprocessing("data/input/test")
+    print(f"1/6 completed in: {t // 60:.0f} min {t % 60:.0f} sec")
+    t += preprocessing("data/input/train")
+    print(f"2/6 completed in: {t // 60:.0f} min {t % 60:.0f} sec")
+    t += preprocessing("data/labels/test/QCANet")
+    print(f"3/6 completed in: {t // 60:.0f} min {t % 60:.0f} sec")
+    t += preprocessing("data/labels/train/NDN")
+    print(f"4/6 completed in: {t // 60:.0f} min {t % 60:.0f} sec")
+    t += preprocessing("data/labels/train/NSN")
+    print(f"5/6 completed in: {t // 60:.0f} min {t % 60:.0f} sec")
+    t += preprocessing("data/labels/train/QCANet")
+    print(f"6/6 completed in: {t // 60:.0f} min {t % 60:.0f} sec")
