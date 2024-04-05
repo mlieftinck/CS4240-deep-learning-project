d48f4cab7cad810f34e984794d7977f7ef0811aa
RaelHuizenga <r.r.huizenga@student.tudelft.nl>
2024-04-05 15:57:46 +0200

    Make it work in kaggle

--- code/main.py
+++ code/main.py
@@ -9,44 +9,36 @@
 # import os
 #
 #
-# def run_training(project_dir, model="NSN", save=False):
+# def run_training(images, labels, test_images, test_labels, model="NSN", save=False):
+#     print(f'------------ RUNNING ------------')
 #     NDN = False
 #     if model == "NDN":
 #         NDN = True
-#     images, labels = preprocessing(model, project_dir)
-#     print(f'------------ RUNNING ------------')
-#     images = images.float()
-#     labels = labels.long()
-#     if NDN:
-#         net = ndn()
-#     else:
-#         net = nsn()
+#
+#     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #
 #     if NDN:
+#         net = ndn().to(device)
 #         optimizer = torch.optim.Adam(net.parameters(), lr=5e-1)
 #     else:
+#         net = nsn().to(device)
 #         optimizer = torch.optim.SGD(net.parameters(), lr=5e-1)
 #     criterion = DiceLoss()
 #
 #     time_start = time.time()
-#     train_loss, train_acc = train(images, labels, net, optimizer, criterion)
-#     test_loss, test_acc, output_array = test(images, labels, net, criterion)
+#     train_loss, train_acc = train(images, labels, net, optimizer, criterion, device)
+#     #     test_loss, test_acc, output_array = test(images, labels, net, criterion)
 #     time_end = time.time() - time_start
 #
-#     print(output_array.size())
-#     print(f'test loss: {test_loss}, test accuracy = {test_acc}')
+#     print(f'test loss: {train_loss}, test accuracy = {train_acc}')
 #     print(f"Total elapsed time: {time_end // 60:.0f} min {time_end % 60:.0f} sec")
 #
 #     # Save trained model
 #     if save:
 #         parent_directory = os.path.dirname(os.getcwd())
-#         os.makedirs(parent_directory + '/trained_models/', exist_ok=True)
-#         torch.save(net.state_dict(), parent_directory + '/trained_models/' + model + '.pth')
-#
-#     view_image(output_array.cpu().detach().numpy()[0])
-#     # view_image(labels[0].cpu().detach().numpy()[0])
-#
-#     return output_array.cpu().detach().numpy()
+#         os.makedirs(parent_directory + '/working/trained_models/', exist_ok=True)
+#         torch.save(net.state_dict(), parent_directory + '/working/trained_models/' + model + '.pth')
+#     return train_loss, train_acc
 #
 #
 # def load_trained_model(model, model_name):
